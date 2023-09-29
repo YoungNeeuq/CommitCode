e66b68143333877eaf5b3f658efb3437b8353068
YoungNeeuq <haulv.dev@gmail.com>
2023-09-29 08:57:47 +0700

    v1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import Nav from "./Components/Nav"
+import Body from "./Components/Body"
+import Footer from "./Components/Footer"
+export default function App() {
+  return (
+    <div className="p-0">
+      <Nav></Nav>
+      <Body></Body>
+      <Footer></Footer>
+    </div>
+  )
+}
